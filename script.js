4699c23f21527762f7ba8176b23e51d4efd6cf5e
learnit <learnit.contactus@gmail.com>
2022-07-17 12:57:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+function select(){
+    if (parseInt(Class.value)==1) {
+        alert(hi1)
+    } else {
+        
+    }
+}
+
+function func1() {
+    document.getElementById('star01').style.color = 'blue';
+    document.getElementById('star02').style.color = 'black';
+    document.getElementById('star03').style.color = 'black';
+    document.getElementById('star04').style.color = 'black';
+    document.getElementById('star05').style.color = 'black';
+    document.getElementById('wsh').innerText='Thanks for rating us 1 star. We will try to improve and get another four stars also.'
+}
+function func2() {
+    document.getElementById('star01').style.color = 'blue';
+    document.getElementById('star02').style.color = 'blue';
+    document.getElementById('star03').style.color = 'black';
+    document.getElementById('star04').style.color = 'black';
+    document.getElementById('star05').style.color = 'black';
+    document.getElementById('wsh').innerText='Thanks for rating us 2 stars. We will try to improve and get another three stars also.'
+}
+function func3() {
+    document.getElementById('star01').style.color = 'blue';
+    document.getElementById('star02').style.color = 'blue';
+    document.getElementById('star03').style.color = 'blue';
+    document.getElementById('star04').style.color = 'black';
+    document.getElementById('star05').style.color = 'black';
+    document.getElementById('wsh').innerText='Thanks for rating us 3 stars. We will try to improve and get another two stars also.'
+}
+function func4() {
+    document.getElementById('star01').style.color = 'blue';
+    document.getElementById('star02').style.color = 'blue';
+    document.getElementById('star03').style.color = 'blue';
+    document.getElementById('star04').style.color = 'blue';
+    document.getElementById('star05').style.color = 'black';
+    document.getElementById('wsh').innerText='Thanks for rating us 4 stars. We will try to improve and get another one star also.'
+}
+function func5() {
+    document.getElementById('star01').style.color = 'blue';
+    document.getElementById('star02').style.color = 'blue';
+    document.getElementById('star03').style.color = 'blue';
+    document.getElementById('star04').style.color = 'blue';
+    document.getElementById('star05').style.color = 'blue';
+    document.getElementById('wsh').innerText='Thanks for rating us 5 stars.'
+}
